97d21ebb8475c29e2e14e1352084f3fffd40c83e
Alfredo Miranda <alfredo@miranda.io>
2023-01-14 21:33:15 +0000

    configure LSP server for lua

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -4,9 +4,24 @@
 -- when using a version manager like asdf
 require('mason').setup()
 
-require('lspconfig').tsserver.setup{
-  on_attach = function()
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer = 0})
-  end,
+local lspconfig = require('lspconfig')
+
+local on_attach = function()
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer = 0})
+end
+
+lspconfig.tsserver.setup{
+  on_attach = on_attach
+}
+
+lspconfig.sumneko_lua.setup{
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
+    }
+  }
 }
