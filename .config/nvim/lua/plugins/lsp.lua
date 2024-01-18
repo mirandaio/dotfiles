d38d7d2dd7165b77ac947c3db72171495f340e5b
Alfredo Miranda <alfredo@miranda.io>
2024-01-18 19:58:41 +0000

    add mapping for hover info

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -17,6 +17,7 @@ return {
       local on_attach = function()
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer = 0})
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer = 0})
       end
 
       lspconfig.tsserver.setup{
