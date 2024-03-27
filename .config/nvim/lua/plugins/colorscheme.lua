134c47b755d3d9b9d303b8751ad10fada48f9719
Alfredo Miranda <alfredo.miranda@utexas.edu>
2024-03-27 13:08:44 +0000

    add tokyonight color scheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -11,5 +11,16 @@ return {
       })
       vim.cmd.colorscheme('rose-pine')
     end
+  },
+  {
+    'folke/tokyonight.nvim',
+    config = function()
+      require('tokyonight').setup({
+        styles = {
+          comments = { italic = false },
+          keywords = { italic = false }
+        }
+      })
+    end
   }
 }
