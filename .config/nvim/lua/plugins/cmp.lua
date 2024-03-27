004bbbe9d4ff02f11a94eeaa2f6a0fd611fab90b
Alfredo Miranda <alfredo.miranda@utexas.edu>
2024-03-27 15:13:49 +0000

    add mappings for nvim-cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -18,7 +18,14 @@ return {
           -- Select the [p]revious item
           ['<C-p>'] = cmp.mapping.select_prev_item(),
           -- Accept ([y]es) the completion
-          ['<C-y>'] = cmp.mapping.confirm { select = true }
+          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          -- Scroll the documentation window [b]ack / [f]orward
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scrool_docs(4),
+          -- Manually trigger a completion from nvim-cmp.
+          -- Generally you don't need this, because nvim-cmp will display
+          -- completions whenever it has completion options available.
+          ['<C-Space>'] = cmp.mapping.complete()
         },
 
         sources = {
