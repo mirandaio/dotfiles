3d01164fcdc872e934898641ff5e359629580eab
Alfredo Miranda <alfredo.miranda@signal-ai.com>
2024-06-14 15:40:26 +0100

    configure snippets

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -3,13 +3,23 @@ return {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path'
     },
     config = function()
       local cmp = require'cmp'
+      local luasnip = require'luasnip'
+      luasnip.config.setup {}
 
       cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+
         completion = { completeopt = 'menu,menuone,noinsert' },
 
         window = {
@@ -35,6 +45,7 @@ return {
 
         sources = {
           { name = 'nvim_lsp' },
+          { name = 'luasnip' },
           { name = 'path' }
         },
 
